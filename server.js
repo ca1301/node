6535f6e76a639125e2332d03c93b4ea9a348dd05
ca1301 <34408387+ca1301@users.noreply.github.com>
2020-10-25 20:35:40 +1100

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) =>{
+    console.log('A user connected');
+    socket.on('disconnect', () =>{
+        console.log('A user disconnected');
+    })
+
+    socket.on('chat message', (msg) => {
+        console.log('Message: ' + msg);
+    })
+})
+
+http.listen(5000, () => {
+  console.log('listening on *:3000');
+});
